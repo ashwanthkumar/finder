67cb3f8c9aea0a1651a0334be43c8cc0b4658c31
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-01-17 01:59:40 +0530

    Implemented a get and moving all query param to path since they're mandatory

--- services/src/main/scala/finder/http/DatasetController.scala
+++ services/src/main/scala/finder/http/DatasetController.scala
@@ -2,6 +2,7 @@ package finder.http
 
 import com.twitter.util.Future
 import finder.config.DatasetConfig
+import finder.index.IndexReader._
 import finder.util.Utils._
 
 class DatasetController(config: DatasetConfig) {
@@ -13,5 +14,12 @@ class DatasetController(config: DatasetConfig) {
         managed(config.recordReader[Any](record))(_.read())
       })
     }
+  }
+
+  def get(id: String, ts: Long): Future[Option[_]] = {
+    search(s"$id$ID_TS_SEPARATOR$ts").map(_.headOption).map ({
+      case Some(datum) if config.dataset.timestamp(datum) == ts => Some(datum)
+      case _ => None
+    })
   }
 }
--- services/src/main/scala/finder/http/DatasetRouter.scala
+++ services/src/main/scala/finder/http/DatasetRouter.scala
@@ -1,15 +1,25 @@
 package finder.http
 
-import com.twitter.finatra.Controller
+import com.twitter.finatra.{Request, Controller}
 import finder.config.FinderConfigReader
+import finder.index.IndexReader
+import finder.index.IndexReader.ID_TS_SEPARATOR
 
 
 class DatasetRouter extends Controller {
   val config = FinderConfigReader.load()
-  get("/:dataset/search") { request =>
+  get("/:dataset/search/:key") { request =>
     val dataset = request.routeParams("dataset")
-    val key = request.params("key")
-    new DatasetController(config.dataset(dataset)).search(key).map(render.json)
+    val identifier = request.routeParams("key")
+    new DatasetController(config.dataset(dataset)).search(identifier).map(render.json)
+  }
+
+  get("/:dataset/get/:id/:timestamp") { request =>
+    val dataset = request.routeParams("dataset")
+    val identifier = request.routeParams("id")
+    val timestamp = request.routeParams("timestamp").toLong
+
+    new DatasetController(config.dataset(dataset)).get(identifier, timestamp).map(render.json)
   }
 
   error { request =>
