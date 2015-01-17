7fb01e582d76b8dce7ce18bea678aaafcd06ea65
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-01-17 09:45:50 +0530

    adding publish settings for finder-core

--- project/Build.scala
+++ project/Build.scala
@@ -7,17 +7,17 @@ object Build extends Build {
   val ScalaVersion = "2.10.4"
 
   lazy val main = Project("finder", file("."), settings = defaultSettings)
-    .aggregate(core)
+    .aggregate(core, services)
 
-  lazy val core = Project("finder-core", file("core"), settings = defaultSettings)
+  lazy val core = Project("finder-core", file("core"), settings = defaultSettings ++ publishSettings)
     .settings(
       libraryDependencies ++= Seq(
         "com.typesafe" % "config" % "1.2.1",
         "org.iq80.leveldb" % "leveldb" % "0.7",
         "com.github.tototoshi" % "scala-csv_2.10" % "1.1.2",
-        "com.hadoop.gplcompression" % "hadoop-lzo" % "0.4.19",
-        "org.apache.hadoop" % "hadoop-client" % "2.0.0-mr1-cdh4.2.1",
         "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.4.4",
+        "com.hadoop.gplcompression" % "hadoop-lzo" % "0.4.19" % "provided",
+        "org.apache.hadoop" % "hadoop-client" % "2.0.0-mr1-cdh4.2.1" % "provided",
         "org.scalatest" %% "scalatest" % "2.2.0" % "test"
       )
     )
@@ -37,5 +37,42 @@ object Build extends Build {
     resolvers += "Cloudera Repo" at "https://repository.cloudera.com/cloudera/cloudera-repos/",
     parallelExecution in This := false
   )
+
+  lazy val publishSettings = Seq(
+    mappings in (Compile, packageBin) ~= (_.filterNot{case (file, _) => file.isDirectory && file.getName == "my-config"}),
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    publishArtifact in(Compile, packageDoc) := true,
+    publishArtifact in(Compile, packageSrc) := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra := _pomExtra
+  )
+
+  val _pomExtra =
+    <url>http://github.com/ashwanthkumar/finder</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:ashwanthkumar/finder.git</url>
+        <connection>scm:git:git@github.com:ashwanthkumar/finder.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>ashwanthkumar</id>
+          <name>Ashwanth Kumar</name>
+          <url>http://www.ashwanthkumar.in/</url>
+        </developer>
+      </developers>
 
 }
